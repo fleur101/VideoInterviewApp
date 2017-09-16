76c91030b908b9472a104fa211ed5678b139805e
Alisher <finesome@gmail.com>
2017-09-16 13:24:54 +0600

    alisher's commit - created layout of MainActivity and transition to TestActivity

--- app/src/main/java/com/mirka/app/naimi/MainActivity.java
+++ app/src/main/java/com/mirka/app/naimi/MainActivity.java
@@ -1,9 +1,16 @@
 package com.mirka.app.naimi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWelcomeMessage;
+    private Button mInterviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // comment commit
+        mWelcomeMessage = (TextView) findViewById(R.id.tv_welcome_message);
+        mInterviewButton = (Button) findViewById(R.id.btn_start_interview);
+
+        mWelcomeMessage.setText(R.string.welcome_message);
+    }
+
+    public void onInterviewButtonClick(View view) {
+        Intent intent = new Intent(MainActivity.this, TestActivity.class);
+        startActivity(intent);
     }
 }

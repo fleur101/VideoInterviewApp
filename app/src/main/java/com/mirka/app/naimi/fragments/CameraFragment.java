0ebe90d9c79f3d217c977806d587391cf2f28f66
Alisher <finesome@gmail.com>
2017-09-16 14:49:47 +0600

    alisher's commit - working on camera preview

--- app/src/main/java/com/mirka/app/naimi/fragments/CameraFragment.java
+++ app/src/main/java/com/mirka/app/naimi/fragments/CameraFragment.java
@@ -6,15 +6,22 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.mirka.app.naimi.R;
 import com.mirka.app.naimi.TestActivity;
+import com.mirka.app.naimi.utils.CameraPreview;
 
 public class CameraFragment extends Fragment {
+
+    private static final String TAG = "CameraFragmentTag";
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -31,9 +38,13 @@ public class CameraFragment extends Fragment {
         // inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
-        if (checkCameraHardware(getContext())) {
-            Camera camera = getCameraInstance();
+        FrameLayout preview = (FrameLayout) view.findViewById(R.id.camera_preview);
 
+        if (checkCameraHardware(getContext())) {
+            mCamera = getCameraInstance();
+
+            mPreview = new CameraPreview(getContext(), mCamera);
+            preview.addView(mPreview);
         } else {
             Toast.makeText(getContext(), "Camera is not supported", Toast.LENGTH_SHORT).show();
         }
@@ -53,12 +64,14 @@ public class CameraFragment extends Fragment {
     /** A safe way to get an instance of the Camera object. */
     public static Camera getCameraInstance(){
         Camera c = null;
+        Log.e(TAG, "getCameraInstance()");
         try {
             // we suppose it
-            c = Camera.open(Camera.getNumberOfCameras() - 1); // attempt to get a Camera instance
-        }
-        catch (Exception e){
+            c = Camera.open(); // attempt to get a Camera instance
+            Log.e(TAG, "getCameraInstance() try");
+        } catch (Exception e){
             // Camera is not available (in use or does not exist)
+            Log.e(TAG, "getCameraInstance() catch");
         }
         return c; // returns null if camera is unavailable
     }
